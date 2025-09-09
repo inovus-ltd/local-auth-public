333f7c7b26f18808583b57900d2cc87915ba2a01
justin-inovus <justin.cann@inovus.org>
2025-09-09 13:39:38 +0100

    removed libs

--- dist/contexts/AuthContext.d.ts
+++
@@ -1,9 +0,0 @@
-import { default as React, ReactNode } from 'react';
-import { AuthContextType } from '../types/auth';
-interface AuthProviderProps {
-    children: ReactNode;
-}
-export declare const AuthProvider: React.FC<AuthProviderProps>;
-export declare const useAuth: () => AuthContextType;
-export {};
-//# sourceMappingURL=AuthContext.d.ts.map
